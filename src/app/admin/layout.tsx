1ba293277d855c02810e26392392c7a7c77227de
Matt <wilsonmatthew@yahoo.com>
2026-02-20 08:58:15 -0700

    Remove secondary page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -25,7 +25,7 @@ export default function AdminLayout({
                 Dashboard
               </Link>
               <Link
-                href="/utah-temples"
+                href="/"
                 className="text-gray-700 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium transition-colors"
               >
                 View Temples
