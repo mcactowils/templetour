2572f65439c42acfb25dd756fd5b3d1869c17a6c
Matt <wilsonmatthew@yahoo.com>
2026-02-24 14:31:40 -0700

    Update

--- src/app/schedules/[id]/edit/page.tsx
+++ src/app/schedules/[id]/edit/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react'
 import { useRouter, useParams } from 'next/navigation'
 import { useSession } from 'next-auth/react'
+import { isAdminUser } from '../../../lib/admin'
 
 interface Temple {
   id: string
